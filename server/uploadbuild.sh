644d48a23ea42514ae83fc929cdc49c1ebd357f5
Björn Ritzl <bjorn.ritzl@king.com>
2017-08-19 11:17:17 +0200

    Added version arg

--- server/uploadbuild.sh
+++ server/uploadbuild.sh
@@ -1,1 +1,3 @@
-aws gamelift upload-build --operating-system AMAZON_LINUX --build-root out --name BroadsockGamelift --build-version 1 --region eu-central-1
+VERSION=${1:-1}
+
+aws gamelift upload-build --operating-system AMAZON_LINUX --build-root out --name BroadsockGamelift --build-version $(VERSION) --region eu-central-1
