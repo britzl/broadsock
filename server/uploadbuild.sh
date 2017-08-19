dabfeb6e319fa2a5d3dfdce800eec3aa212ba432
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-08-19 11:20:51 +0200

    Fixed error in upload script

--- server/uploadbuild.sh
+++ server/uploadbuild.sh
@@ -1,3 +1,3 @@
-VERSION=${1:-1}
+VERSION=${1:-"1"}
 
-aws gamelift upload-build --operating-system AMAZON_LINUX --build-root out --name BroadsockGamelift --build-version $(VERSION) --region eu-central-1
+aws gamelift upload-build --operating-system AMAZON_LINUX --build-root out --name BroadsockGamelift --build-version $VERSION --region eu-central-1
