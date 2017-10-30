5e902779c99543a0c3f9d6b53d2bd280220a260d
Björn Ritzl <bjorn.ritzl@gmail.com>
2017-10-30 13:15:46 +0100

    Removed cpp server

--- server/gamelift_uploadbuild.sh
+++
@@ -1,4 +0,0 @@
-VERSION=${1:-"1"}
-REGION=${2:-"eu-central-1"}
-
-aws gamelift upload-build --operating-system AMAZON_LINUX --build-root out --name BroadsockGamelift --build-version $VERSION --region $REGION
